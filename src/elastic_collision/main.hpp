9cf06dc2b79c615e83498eccbe729c83fbf8b88f
braga <pedro.reis@mds.gov.br>
2024-08-14 10:35:18 -0300

    Adjusting header files for fallen balls and elastic colision

---
+++ src/elastic_collision/main.hpp
@@ -0,0 +1,9 @@
+#ifndef ELASTIC_COLLISION_HPP
+#define ELASTIC_COLLISION_HPP
+
+#include <SFML/Graphics.hpp>
+#include "main.cpp"
+
+void handleElasticCollision(sf::RenderWindow &window, Screen &currentScreen);
+
+#endif // ELASTIC_COLLISION_HPP
---
+++ src/fallen_balls/main.hpp
@@ -0,0 +1,9 @@
+#ifndef FALLEN_BALLS_HPP
+#define FALLEN_BALLS_HPP
+
+#include <SFML/Graphics.hpp>
+#include "main.cpp"
+
+void handleBounceBall(sf::RenderWindow &window, Screen &currentScreen);
+
+#endif // FALLEN_BALLS_HPP
